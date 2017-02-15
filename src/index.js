ee2fc0c0925f1ff3992750374ba987688a929270
madebymak <mr.jmak@gmail.com>
2017-02-15 15:32:55 -0800

    Fixed errors.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './Routes.js';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 render(
-  <Route routes={routes} history={browserHistory} />
+  <Router routes={routes} history={browserHistory} />,
   document.getElementById('app')
 );

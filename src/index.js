509ba274444855abcdac0faf2fccb170d6f796b5
madebymak <mr.jmak@gmail.com>
2017-02-15 15:17:51 -0800

    Set up entry point for routes.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-// Set up your application entry point here...
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './Routes.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+  <Route routes={routes} history={browserHistory} />
+  document.getElementById('app')
+);

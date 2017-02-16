8590a380b711768f3481ffa79907e282b630ecce
madebymak <mr.jmak@gmail.com>
2017-02-15 20:22:35 -0800

    Working template.

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './Routes.js';
 import configureStore from './store/configureStore';
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
 
 render(
   <Provider store={store}>
-    <Router routes={routes} history={browserHistory} />,
+    <Router routes={routes} history={browserHistory} />
   </Provider>,
   document.getElementById('app')
 );
